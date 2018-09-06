aaef1e994a21c33533fb7dab3fc4764a22bb52b3
Gennady Sadchikov <dessolo@mail.ru>
2018-09-06 16:41:40 +0300

    Add example

--- example.py
+++ example.py
@@ -1,11 +1,13 @@
 from tgbotapi import TgBot
 
-bot = TgBot(token='sdsdsd')
-#bot.settings = {'proxies': {'https': 222}}
-#bot.settings['proxies'] = {'https': 222}
-#message = bot.send_message(-1001338600997, 4)
-#print(message.text)
-bot.forward_message(-1001338600997, -1001338600997, 746)
-f = bot.send_photo(-1001338600997, '/home/solo/Изображения/Выделение_007.jpg')
-f = bot.send_photo(-1001338600997, 'https://pbs.twimg.com/profile_images/1019623145471295488/SZCsC2Ci_400x400.jpg')
-print(f.text)
+bot = TgBot(token='TOKEN')
+bot.settings = {'proxies': {'https': 'https://USERNAME:PASSWORD@IP:PORT'}}
+bot.settings['proxies'] = {'https': 'https://USERNAME:PASSWORD@IP:PORT'}
+message = bot.send_message(-1001338600997, 'Hello')
+print(message.text)
+message = bot.forward_message(-1001338600997, -1001338600997, 1) # 1 - id message
+print(message.text)
+photo = bot.send_photo(-1001338600997, '/PATH/TO/FILE/SOME_FILE')
+print(photo.text)
+photo = bot.send_photo(-1001338600997, 'https://pbs.twimg.com/profile_images/1019623145471295488/SZCsC2Ci_400x400.jpg')
+print(photo.text)
