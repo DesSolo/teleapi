7f6f0972f9158306b14562da1a98c437c0acac90
DesSolo <dessolo@mail.ru>
2020-04-29 01:24:54 +0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+
+def read(file_name):
+    with open(file_name, 'r') as file:
+        return file.read()
+
+
 setup(
     name='teleapi',
     version='0.1.3',
@@ -7,9 +13,9 @@ setup(
     author_email='dessolo@mail.ru',
     url='https://github.com/DesSolo/teleapi',
     description='Simple telegram api',
-    license='GPL2',
-    long_description='Simple telegram api',
+    license='GPLv3+',
+    long_description=read('README.md'),
     keywords='telegram bot api',
     packages=find_packages(),
-    install_requires=['requests', 'pysocks'],
+    install_requires=['requests', 'pysocks']
 )
